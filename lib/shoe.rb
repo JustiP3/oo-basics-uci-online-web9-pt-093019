bdd7b8d57468a0420cf2b1252f9c3d10d186a347
Justin Phillips <justinphil02@yahoo.com>
2019-10-23 18:54:37 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,8 +7,10 @@ attr_writer :brand, :color, :size, :material, :condition
 attr_reader :brand, :color, :size, :material, :condition
 
 def cobble
+  :condition = "new"
   puts "Your shoe is as good as new!"
-end 
+end
+
 
 
 end
