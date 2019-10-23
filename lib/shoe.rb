37d52458680945193954da4269790420df5acc5e
Justin Phillips <justinphil02@yahoo.com>
2019-10-23 18:59:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,14 +3,14 @@ class Shoe
 def initialize(brand)
   @brand = brand
 end
-attr_writer :brand, :color, :size, :material
-attr_reader :brand, :color, :size, :material
-def condition
-  @condition
-end
-def condition=(status)
-  @condition = status
-end
+attr_writer :brand, :color, :size, :material, :condition
+attr_reader :brand, :color, :size, :material, :condition
+#def condition
+#  @condition
+#end
+#def condition=(status)
+#  @condition = status
+#end
 
 
 def cobble
